4b4c725a9f1ee13a6dbbfcecb8b384dfb62d99f3
LeeHyeMin98 <tlk6@naver.com>
2020-12-31 13:19:25 +0900

    로그인 디자인

---
+++ design/login/login_form.js
@@ -0,0 +1,6 @@
+$('.login_m').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+$('.create_m').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
